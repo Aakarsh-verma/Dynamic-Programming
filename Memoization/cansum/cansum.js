90476756da79a9790335d6953d0745e7cc2a3b71
Aakarsh-verma <aakarshkun@gmail.com>
2021-04-02 07:53:11 +0530

    commit

---
+++ Memoization/cansum/cansum.js
@@ -0,0 +1,37 @@
+
+const notgoodcanSum = (targetSum, numbers) => {
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+
+    for (let num of numbers){
+        const remainder = targetSum - num;
+        if (notgoodcanSum(remainder, numbers) === true){
+            return true;
+        } 
+    }
+
+    return false;
+};
+
+const canSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+
+    for (let num of numbers){
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true){
+            memo[targetSum] = true;
+            return true;
+        } 
+    }
+
+    memo[targetSum] = false;
+    return false;
+};
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
